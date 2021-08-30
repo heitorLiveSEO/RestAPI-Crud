7b9c704eaa9c912add37be734a2c8c5f9b66afec
Heitor LiveSEO <82043987+heitorLiveSEO@users.noreply.github.com>
2021-08-30 16:24:14 -0300

    Update api.bat

--- api.bat
+++ api.bat
@@ -2,4 +2,4 @@ docker-compose up --build -d
 
 cd api 
 
-yarn install && yarn sequelize db:migrate && yarn start
+npm install --global yarn && yarn install && yarn sequelize db:migrate && yarn start
